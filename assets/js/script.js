189d724b760e71b7bbf0204ddea0b0fde0f05f0d
Abuzar545 <aghafari545@gmail.com>
2026-07-01 22:33:08 +0500

    add responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
-$('.owl-carousel').owlCarousel({
+$('.homePromoSlider').owlCarousel({
     loop:true,
     margin:10,
-    nav:false,
+    nav:true,
+    navText: ['<div class="slider-side-arrow">←</>', '<div class="slider-side-arrow">→</>'],
     responsive:{
         0:{
             items:1
@@ -9,3 +10,23 @@ $('.owl-carousel').owlCarousel({
       
     }
 })
+$(document).ready(function () {
+
+    $(".mega-menu-button").click(function () {
+
+        if ($(window).width() >= 992) {
+
+            // Desktop Menu
+            $(".mega-menu").fadeToggle(300);
+            $(".mobile-menu").fadeOut(300);
+
+        } else {
+
+            // Mobile Menu
+            $(".mobile-menu").fadeToggle(300);
+              $(".mega-menu").fadeOut(300);
+        }
+
+    });
+
+});
